d2bd7f8fd5313ac46299a2b5b8b2e91fd4c3505f
SeungsooChoi <tmdtn5389@gmail.com>
2021-11-27 19:15:23 +0900

    refactor : React.memo, useState 함수형 업데이트로 데이터가 많아졌을때 느려지는 현상 최적화

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트 기초 다지기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '앱 만들기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 0; i < 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+
+  return array;
+}
+
+const App = () => {
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트 기초 다지기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '앱 만들기',
+  //     checked: false,
+  //   },
+  // ]);
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // id는 렌더링되는 정보가 아니므로 useRef로 관리
   const nextId = useRef(4);
+  // const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
@@ -33,30 +48,25 @@ function App() {
         checked: false,
       };
 
-      setTodos(todos.concat(todo)); // 새로운 배열을 만들어서 setTodos
+      setTodos((todos) => todos.concat(todo)); // 새로운 배열을 만들어서 setTodos
       nextId.current++;
     },
-    [todos], // 함수 안에서 todos에 의존성이 있음.
+    [], // 함수 안에서 todos에 의존성이 있음.
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        // 불변성 유지하면서 특정 배열 원소를 업데이트하기 위해 map 사용
+  const onToggle = useCallback((id) => {
+    setTodos(
+      // 불변성 유지하면서 특정 배열 원소를 업데이트하기 위해 map 사용
+      (todos) =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
-      );
-    },
-    [todos],
-  );
+    );
+  }, []);
 
   return (
     <TodoTemplate>
@@ -64,6 +74,6 @@ function App() {
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-}
+};
 
 export default App;
