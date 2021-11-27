5c4936dd4f50fc651b7ad684cdb3068ea4425074
SeungsooChoi <tmdtn5389@gmail.com>
2021-11-27 19:45:57 +0900

    refactor : react-virtualized로 TodoList 컴포넌트 성능 최적화

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  // List컴포넌트에서 각 TodoItem을 렌더링할때 사용.
+  const rowRenderer = useCallback(({ index, key, style }) => {
+    const todo = todos[index];
+    return (
+      <TodoListItem
+        todo={todo}
+        key={key}
+        onRemove={onRemove}
+        onToggle={onToggle}
+        style={style}
+      />
+    );
+  });
+
   return (
-    <div className="TodoList">
-      {todos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          key={todo.id}
-          onRemove={onRemove}
-          onToggle={onToggle}
-        />
-      ))}
-    </div>
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
+// 리스트 관련 컴포넌트 작성시 리스트 아이템, 리스트 이 두가지는 최적화 해주는것이 좋다.(상황에 따라)
